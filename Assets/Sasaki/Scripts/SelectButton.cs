ab9100a87f13e3853c0a3369859de2df0e7fb506
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2024-05-17 17:23:42 +0900

    シーン遷移追加

--- Assets/Sasaki/Scripts/SelectButton.cs
+++ Assets/Sasaki/Scripts/SelectButton.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectButton : MonoBehaviour
 {
     public static string SelectStage = null;
 
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+    }
 
     //押したボタンの名前を取得
     public void ButtonName(string objName)
     {
-        //小文字表記にする
-        objName = objName.ToLower();
         SelectStage = objName;
+
+        SceneManager.LoadScene("MainScene");
     }
 }
