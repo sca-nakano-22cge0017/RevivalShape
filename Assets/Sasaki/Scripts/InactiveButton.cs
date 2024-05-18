d2d73cbe95e0259f5d8c6a771cedcc048b79700e
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2024-05-18 18:12:29 +0900

    セレクトシーンUI変更

--- Assets/Sasaki/Scripts/InactiveButton.cs
+++ Assets/Sasaki/Scripts/InactiveButton.cs
@@ -5,19 +5,40 @@ using UnityEngine.UI;
 
 public class InactiveButton : MonoBehaviour
 {
-    [SerializeField, Header("使えないボタン")] GameObject[] button;
+    [SerializeField, Header("使えないボタン")] private GameObject[] buttonObj = null;
+    [SerializeField, Header("Content")] private RectTransform content = null;
+    [SerializeField, Header("エキストラボタンを引いた分のContentの縦幅")] private float contentHeight = 400.0f;
+    private GameObject extraButton = null;
 
+    float width = 0.0f;
+    float height = 0.0f;
     void Start()
     {
-        for (int i = 0; i < button.Length; i++)
+        extraButton = GameObject.Find("Extra");
+
+        width = content.sizeDelta.x;
+        height = content.sizeDelta.y;
+        for (int i = 0; i < buttonObj.Length - 2; i++)
         {
-            button[i].GetComponent<Button>().interactable = false;
+            buttonObj[i].GetComponent<Button>().interactable = true;
         }
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < buttonObj.Length - 2; i++)
+        {
+            //星が一定値にいったらエクストラボタンを表示
+            if (buttonObj[i].GetComponent<Button>().interactable == true)
+            {
+                extraButton.SetActive(true);
+                content.sizeDelta = new Vector2(width, height);
+            }
+            else
+            {
+                extraButton.SetActive(false);
+                content.sizeDelta = new Vector2(width, height - contentHeight);
+            }
+        }
     }
 }
