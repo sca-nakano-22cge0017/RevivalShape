4f9f0cb9dea43cd48d8ec9fee3ec77c6af6f85f9
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2024-05-24 17:32:45 +0900

    実行フェーズ　落下調整

--- Assets/Nakano/Scripts/ShapeObjects.cs
+++ Assets/Nakano/Scripts/ShapeObjects.cs
@@ -31,7 +31,7 @@ public class ShapeObjects : MonoBehaviour
         vibration = GameObject.FindObjectOfType<Vibration>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if(!IsFall) return;
 
