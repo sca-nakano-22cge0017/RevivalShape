5c5ffcbcc40e161f1a1c301b0e23dcd8bf3bafd7
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2024-08-23 16:42:22 +0900

    メインに演出追加

--- Assets/Sasaki/Scripts/TapEffectScript.cs
+++ Assets/Sasaki/Scripts/TapEffectScript.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class TapEffectScript : MonoBehaviour
 {
     [SerializeField] private ParticleSystem tapEffect;
-    private Camera mainCamera;
+    [SerializeField] private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main;
+        //mainCamera = Camera.main;
     }
 
     // Update is called once per frame
